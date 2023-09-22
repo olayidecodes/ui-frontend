684dc20a80f0b0695a4f6bc3a0a57f7398d264a4
VICTORIA MATTHEWS <olayidecodes@gmail.com>
2023-09-22 19:36:12 +0100

    added lead the way section

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -2,12 +2,21 @@ import './Banner.scss'
 
 const Banner = () => {
   return (
-    <div className='banner'>
-        <img className='logo_bg' src="/unibadan-logo.png" alt="University of Ibadan" />
-        <div className="content">
-            <h1>Alumni, Lead the Way!</h1>
-            <p>Commemorate 75 years of excellence at the University of Ibadan. Join fellow alumni in shaping the future. Contribute to our crowdfunding campaign today!</p>
-            <button className="btn" >Donate</button>
+    <div className='hero__banner'>
+        <div className="banner">
+            <img className='logo_bg' src="/unibadan-logo.png" alt="University of Ibadan" />
+            <div className="content">
+                <h1>Alumni, Lead the Way!</h1>
+                <p>Commemorate 75 years of excellence at the University of Ibadan. Join fellow alumni in shaping the future. Contribute to our crowdfunding campaign today!</p>
+                <button className="btn" >Donate</button>
+            </div>
+            <div className="countdown">
+
+            </div>
+            <div className="lead section">
+                <h1>LEAD THE WAY</h1>
+                <p>Join us, proud alumni of the University of Ibadan, as we celebrate 75 years of academic excellence and chart a bold course for the future. Your support in this crowdfunding campaign will ensure that our alma mater continues to inspire and educate generations to come. Lead the way!</p>
+            </div>
         </div>
     </div>
   )
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import { FaTwitterSquare, FaLinkedin, FaFacebookSquare } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <nav className="container">
+    <nav className="footer">
         <img src="/logo.webp" alt='University of Ibadan'/>
         <div className="links">
             <a className="link" href="twitter.com"><FaTwitterSquare  /></a>
