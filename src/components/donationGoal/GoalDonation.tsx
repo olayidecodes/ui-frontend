e020450c48e91aca48140dc5d5691ee35e199ca0
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-10-01 02:31:38 +0100

    donation progress bar

--- src/components/donationGoal/GoalDonation.tsx
+++ src/components/donationGoal/GoalDonation.tsx
@@ -2,12 +2,25 @@ import "./donationGoal.css"
 import React from 'react'
 
 const GoalDonation = () => {
+  let goal = "$200,000,000.00";
+  let amountRaised = "$50,986,908.999";
+
   return (
     <div className="goalContainer">
         <div className="toptext">
             <h1>DONATION GOAL</h1>
             <p>Our goal is to reach about Two Hundred million dollars ($200,000,00.00) 
                 before the start of the University’s 75th Anniversary.</p>
+
+            <div className="donationBar">
+              <span className="donate-per">
+                <span className="tooltip">51%</span>
+              </span>
+            </div>
+
+            <div className="raised">
+              <span>{amountRaised} </span>raised of <span>{goal}</span>
+            </div>
         </div>
     </div>
   )
