5289d38b84d5cc111e090cd20fd3468dde2d1040
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-10-30 01:20:11 +0100

    made some changes on the donation progress bar

--- src/components/donationGoal/GoalDonation.tsx
+++ src/components/donationGoal/GoalDonation.tsx
@@ -1,32 +1,62 @@
+import React from 'react';
 import "./donationGoal.css"
-import React from 'react'
 
-const GoalDonation = () => {
-  let goal = "$200,000,000.00";
-  let amountRaised = "$50,986,908.999";
+interface DonationProgressBarProps {
+  goal: any;
+  currentAmount: any
+}
+
+const GoalDonation: React.FC<DonationProgressBarProps> = ({ goal, currentAmount }) => {
+  const currentAmounts = currentAmount.replace(/[^0-9]/g, '');
+  const goals = goal.replace(/[^0-9]/g, ''); // You can set the current amount as needed
+
+  const percentRaised = ((currentAmounts / goals) * 100).toFixed(2);
 
   return (
     <div className="goalContainer">
       <img src="/toppat.png" alt="top pattern" className="toppat"/>
-      <img src="/cornerpat.png" alt="cornerppattern" className="conpat"/>
+       <img src="/cornerpat.png" alt="cornerppattern" className="conpat"/>
         <div className="toptext">
             <h1>DONATION GOAL</h1>
-            <p>Our goal is to reach about Two Hundred million dollars ($200,000,00.00) 
+          <p>Our goal is to reach about Two Hundred million dollars ($200,000,00.00) 
                 before the start of the University’s 75th Anniversary.
-            </p>
+          </p>
         </div>
 
-            <div className="donationBar">
-              <span className="donate-per">
-                <span className="tooltip">51%</span>
-              </span>
-            </div>
+      <div className="progress-container">
+        <div
+          className="progress-bar"
+          style={{
+            width: percentRaised + '%',
+            animationDuration: '5s', // You can adjust the animation speed here
+          }}
+        >
+              <div className="circle1">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 54 54" fill="none">
+                   <circle cx="27.0081" cy="27" r="27" fill="#E56363"/>
+                 </svg>
+               </div>
+               <div className="circle2">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 54 54" fill="none">
+                   <circle cx="27.0081" cy="27" r="27" fill="#E56363"/>
+                 </svg>
+               </div>
 
-            <div className="raised">
-              <span>{amountRaised} </span>raised of <span> {goal}</span>
-            </div>
+               <div className="circle3">
+                 <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 54 54" fill="none">
+                   <circle cx="27.0081" cy="27" r="27" fill="#E56363"/>
+                 </svg>
+               </div>
+
+          <span className="tooltip">{percentRaised}%</span>
+        </div>
+      </div>
+
+      <div className="raised">
+        <span className="amtraised">{currentAmount} /</span><span> {goal}</span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default GoalDonation
+export default GoalDonation;
