dbfdc54600b310ca6ef3475265f986b3eb64a4c6
A-Tech-Services <abiodunoluwapelumi859@gmail.com>
2023-11-16 12:21:54 +0100

    added little update

--- src/components/donationGoal/GoalDonation.tsx
+++ src/components/donationGoal/GoalDonation.tsx
@@ -1,67 +1,3 @@
-// import React from 'react';
-// import "./donationGoal.css"
-
-// interface DonationProgressBarProps {
-//   goal: any;
-//   currentAmount: any
-// }
-
-// const GoalDonation: React.FC<DonationProgressBarProps> = ({ goal, currentAmount }) => {
-//   const currentAmounts = currentAmount.replace(/[^0-9]/g, '');
-//   const goals = goal.replace(/[^0-9]/g, ''); // You can set the current amount as needed
-
-//   const percentRaised = ((currentAmounts / goals) * 100).toFixed(2);
-
-//   return (
-//     <div className="goalContainer">
-//       <img src="/toppat.png" alt="top pattern" className="toppat"/>
-//        <img src="/cornerpat.png" alt="cornerppattern" className="conpat"/>
-//         <div className="toptext">
-//             <h1>DONATION GOAL</h1>
-//           <p>Our goal is to reach about Two Hundred million dollars ($200,000,00.00) 
-//                 before the start of the University’s 75th Anniversary.
-//           </p>
-//         </div>
-
-//       <div className="progress-container">
-//         <div
-//           className="progress-bar"
-//           style={{
-//             '--percentRaised': `${percentRaised}%`,
-//             animationDuration: '5s', // You can adjust the animation speed here
-//           }}
-//         >
-//               <div className="circle1">
-//                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 54 54" fill="none">
-//                    <circle cx="27.0081" cy="27" r="27" fill="#E56363"/>
-//                  </svg>
-//                </div>
-//                <div className="circle2">
-//                  <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 54 54" fill="none">
-//                    <circle cx="27.0081" cy="27" r="27" fill="#E56363"/>
-//                  </svg>
-//                </div>
-
-//                <div className="circle3">
-//                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 54 54" fill="none">
-//                    <circle cx="27.0081" cy="27" r="27" fill="#E56363"/>
-//                  </svg>
-//                </div>
-
-//           <span className="tooltip">{percentRaised}%</span>
-//         </div>
-//       </div>
-
-//       <div className="raised">
-//         <span className="amtraised">{currentAmount} /</span><span> {goal}</span>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default GoalDonation;
-
-
 import React, { useEffect, useState } from 'react';
 import './donationGoal.css';
 
@@ -74,7 +10,7 @@ const GoalDonation: React.FC<DonationProgressBarProps> = ({ goal, currentAmount 
   const currentAmounts = currentAmount.replace(/[^0-9]/g, '');
   const goals = goal.replace(/[^0-9]/g, '');
 
-  const percentRaised = ((currentAmounts / goals) * 100).toFixed(2);
+  const percentRaised = Math.min(100, parseFloat(((currentAmounts / goals) * 100).toFixed(2)));
   const [animatedWidth, setAnimatedWidth] = useState(0);
 
   useEffect(() => {
@@ -115,7 +51,8 @@ const GoalDonation: React.FC<DonationProgressBarProps> = ({ goal, currentAmount 
       </div>
 
       <div className="progress-container">
-        <div className="progress-bar" style={progressBarStyle}>
+        <div className="redfill">
+            <div className="progress-bar" style={progressBarStyle}>
           <div className="circle1">
             <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 54 54" fill="none">
               <circle cx="27.0081" cy="27" r="27" fill="#E56363" />
@@ -137,6 +74,7 @@ const GoalDonation: React.FC<DonationProgressBarProps> = ({ goal, currentAmount 
         </div>
       </div>
 
+        </div>
       <div className="raised">
         <span className="amtraised">{currentAmount} /</span>
         <span> {goal}</span>
--- src/components/table/DonationTable.tsx
+++ src/components/table/DonationTable.tsx
@@ -1,98 +1,67 @@
-import { FaSortDown, FaSortUp } from "react-icons/fa";
-import React, { useState, useEffect } from 'react';
-import "./style.css"
+import React from 'react';
+import "./donateStyle.css";
 
-interface DonationData {
+interface DonationData{
   position: number;
   hallName: string;
-  amountDonated: string;
+  amountDonated: number;
   numOfDonation: number;
-  step: number;
 }
 
 const DonationTable: React.FC = () => {
-  const initialData: DonationData[] = [
-    { position: 1, hallName: 'Kenneth Mellanby Hall', amountDonated: "₦152,342,345.06", numOfDonation: 305, step: 0 },
-    { position: 2, hallName: 'Queen Elizabeth II Hall', amountDonated: "₦152,342,345.06", numOfDonation: 295, step: 0 },
-    { position: 3, hallName: 'Queen Idia Hall', amountDonated: "₦152,342,345.06", numOfDonation: 285, step: 0 },
-    { position: 4, hallName: 'Sultan Bello Hall', amountDonated: "₦152,342,345.06", numOfDonation: 275, step: 0 },
-    { position: 5, hallName: 'Nnamdi Azikwe Hall', amountDonated: "₦152,342,345.06", numOfDonation: 245, step: 0 },
-    { position: 6, hallName: 'Independence Hall', amountDonated: "₦152,342,345.06", numOfDonation: 205, step: 0 },
-    { position: 7, hallName: 'Ransome Kuti Hall', amountDonated: "₦152,342,345.06", numOfDonation: 195, step: 0 },
-    { position: 8, hallName: 'Obafemi Awolowo Hall', amountDonated: "₦152,342,345.06", numOfDonation: 175, step: 0 },
-    { position: 9, hallName: 'Lord Tedder Hall', amountDonated: "₦152,342,345.06", numOfDonation: 155, step: 0 },
-    { position: 10, hallName: 'Alexander Brown Hall', amountDonated: "₦152,342,345.06", numOfDonation: 154, step: 0 },
-    { position: 11, hallName: 'Nnamdi Azikwe Hall2', amountDonated: "₦152,342,345.06", numOfDonation: 145, step: 0 }
+   const leaderBoard: DonationData[] = [
+    { position: 1, hallName: 'Kenneth Mellanby Hall', amountDonated: 152342345.06, numOfDonation: 305 },
+    { position: 2, hallName: 'Queen Elizabeth II Hall', amountDonated: 152342345.06, numOfDonation: 295 },
+    { position: 3, hallName: 'Queen Idia Hall', amountDonated: 152342345.06, numOfDonation: 285 },
+    { position: 4, hallName: 'Sultan Bello Hall', amountDonated: 152342345.06, numOfDonation: 275 },
+    { position: 5, hallName: 'Nnamdi Azikwe Hall', amountDonated: 152342345.06, numOfDonation: 245 },
+    { position: 6, hallName: 'Independence Hall', amountDonated: 152342345.06, numOfDonation: 205 },
+    { position: 7, hallName: 'Ransome Kuti Hall', amountDonated: 152342345.06, numOfDonation: 195 },
+    { position: 8, hallName: 'Obafemi Awolowo Hall', amountDonated: 152342345.06, numOfDonation: 175 },
+    { position: 9, hallName: 'Lord Tedder Hall', amountDonated: 152342345.06, numOfDonation: 155 },
+    { position: 10, hallName: 'Alexander Brown Hall', amountDonated: 152342345.06, numOfDonation: 154 },
+    { position: 11, hallName: 'Post graduate Hall', amountDonated: 152342345.06, numOfDonation: 145 }
   ];
 
+  // Step 1: Sort the leaderBoard array based on amountDonated
+  const sortedLeaderBoard = [...leaderBoard].sort((a, b) => b.amountDonated - a.amountDonated);
 
-  const [sortedData, setSortedData] = useState<DonationData[]>(initialData);
 
-  useEffect(() => {
-    // Sort data by amount donated in descending order
-    const sorted = [...sortedData].sort((a, b) =>
-      parseInt(b.amountDonated.replace(/[^0-9]/g, ''), 10) - parseInt(a.amountDonated.replace(/[^0-9]/g, ''), 10)
-    );
+  // Step 2: Update the position property
+  const updatedLeaderBoard = sortedLeaderBoard.map((data, index) => ({
+    ...data,
+    position: index + 1,
+  }));
 
-    // Update positions and steps
-    const updatedData = sorted.map((row, index) => {
-      let steps = 0;
-      if (index > 0) {
-        if (parseInt(row.amountDonated.replace(/[^0-9]/g, ''), 10) === parseInt(sortedData[index - 1].amountDonated.replace(/[^0-9]/g, ''), 10)) {
-          steps = sortedData[index - 1].step;
-        } else {
-          steps = sortedData[index - 1].step + 1;
-        }
-      } else {
-        steps = 0; // For the first row, steps should be 0
-      }
-      return { ...row, position: index + 1, step: steps };
-    });
-
-    setSortedData(updatedData);
-  }, [sortedData]);
-
+  
   return (
-    <div className="tableContainer">
-      <ul className="responsive-table">
-        <li className="table-header">
-          <div className="col col-1">Position</div>
-          <div className="col col-2">Hall Of Residence</div>
-          <div className="col col-3">Amount Donated</div>
-          <div className="col col-3">Number Of Donations</div>
-          <div className="col col-4">Steps</div>
-        </li>
-        {sortedData.map((row, index) => {
-          let movementContent = null;
-          let steps = 0;
-    
-          if (index > 0) {
-            steps = row.step - sortedData[index - 1].step;
-            if (steps > 0) {
-              movementContent = <FaSortUp style={{ color: 'green' }} />;
-            } else if (steps < 0) {
-              movementContent = <FaSortDown style={{ color: 'red' }} />;
-            }
+    <div className='leaderBoardContainer'>
+      <div className="leaderBoard">
+        <ul className='boardHead'>
+          <li>Position</li>
+          <li>Hall of Residence</li>
+          <li>Amount Donated</li>
+          <li>Number of Donations</li>
+        </ul>
+      
+        <div className="gaps">
+          {/* loopiing through the leader board detials */}
+          {
+            updatedLeaderBoard.map((data) => {
+              return(
+                <ul className='boardBody' key={data.position}>
+                  <li>{data.position}</li>
+                  <li className='left'>{data.hallName}</li>
+                  <li className='left2'>₦{data.amountDonated.toLocaleString("en-US")}</li>
+                  <li>{data.numOfDonation}</li>
+                </ul>
+              )
+            })
           }
-    
-          return (
-            <li className="table-row" key={row.position}>
-              <div className="col col-1" data-label="Job Id">{row.position}</div>
-              <div className="col col-2" data-label="Customer Name">{row.hallName}</div>
-              <div className="col col-3" data-label="Amount">{row.amountDonated}</div>
-              <div className="col col-4" data-label="Payment Status">{row.numOfDonation}</div>
-              <div className="col col-5" data-label="Payment Status">{movementContent && (
-                <>
-                  {movementContent} {Math.abs(steps)} step{Math.abs(steps) !== 1 ? 's' : ''}
-                </>
-              )}
-              </div>
-            </li>
-          );
-        })}
-      </ul>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default DonationTable;
+export default DonationTable
